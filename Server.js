589288731b37f5b4f1c5f08ffafd9ccd03b4fada
unknown <alirezang7575@gmial.com>
2023-11-11 10:57:25 +0330

    added not found page

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ const app= express();
 const statics=require("./utils/Static")
 const route=require("./Routes/admintodoroutes")
 const listrouter=require("./Routes/todoListRoutes")
+const fourofour=require("./Controller/404")
 //EJS
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -16,7 +17,7 @@ statics.static(app);
 app.use(listrouter);
 
 app.use("/admin",route)
-
+ app.use(fourofour.handler)
 app.listen(3000,()=>{
     console.log("server is runnig")
 })
