9f4d6b2417955b34237d34350d6bdb614419ca56
unknown <alirezang7575@gmial.com>
2023-11-12 12:41:20 +0330

    use async methods in our project

--- Controller/adminTodoContorller.js
+++ Controller/adminTodoContorller.js
@@ -6,33 +6,48 @@ const Todo = require("../model/Todo");
 const app = ex();
 
 
-exports.todoadmincontroller = (req, res) => {
+exports.todoadmincontroller = async (req, res) => {
     if (!req.body.todo) return res.redict("/")
     else {
-        Todos.create({ text: req.body.todo }).then(result => {
-            res.redirect("/")
+
+        try {
+            await Todos.create({ text: req.body.todo });
+            res.redirect("/");
+        } catch (error) {
+            console.error(error)
+
         }
-        ).catch(error => {
-            console.log(console);
-        })
 
     }
 }
-exports.deletetodo = (req, res) => {
-    Todo.destroy({ where: { id: req.params.id } }).then(result => {
+exports.deletetodo = async (req, res) => {
+
+
+    try {
+        await Todo.destroy({ where: { id: req.params.id } })
         res.redirect("/");
-    }).catch(err => {
-        console.log(err)
-    })
+    } catch (error) {
+        console.error(error)
+    }
 }
 
-exports.compeleted = (req, res) => {
-    Todo.findByPk(req.params.id).then((todo) => {
-        todo.compeleted = true;
-        todo.save()
-    }).then((result) => {
-        res.redirect("/")
-    }).catch(err => {
-        console.log(err)
-    })
+exports.compeleted = async (req, res) => {
+    // Todo.findByPk(req.params.id).then((todo) => {
+    //     todo.compeleted = true;
+    //     todo.save()
+    // }).then((result) => {
+    //     res.redirect("/")
+    // }).catch(err => {
+    //     console.log(err)
+    // })
+
+
+     try {
+        const todoforupdate= await Todo.findByPk(req.params.id)
+        todoforupdate.compeleted=true;
+         await todoforupdate.save();
+         res.redirect("/")
+     } catch (error) {
+        console.error(error)
+     }
 }
--- Controller/listtodos.js
+++ Controller/listtodos.js
@@ -1,23 +1,39 @@
+const Todo = require("../model/Todo")
 const model = require("../model/Todo")
-module.exports.listcontroler = (req, res) => {
-    model.count({where:{compeleted:true}}).then(trues=>{
-        model.count({where:{compeleted:false}}).then( falses=>{
-            model.findAll().then(todos => {
-                res.render("index",
-                    {
-                        pageTitle: "کارهای روزمره",
-                        todos: todos,
-                        truetodo: trues,
-                        falsetodo:  falses
-                    }
-                );
-        
-            }).catch(err => { console.log(err) })
-        }
+module.exports.listcontroler = async (req, res) => {
+    // model.count({where:{compeleted:true}}).then(trues=>{
+    //     model.count({where:{compeleted:false}}).then( falses=>{
+    //         model.findAll().then(todos => {
+    //             res.render("index",
+    //                 {
+    //                     pageTitle: "کارهای روزمره",
+    //                     todos: todos,
+    //                     truetodo: trues,
+    //                     falsetodo:  falses
+    //                 }
+    //             );
 
-        ).catch(err => { console.log(err) })
+    //         }).catch(err => { console.log(err) })
+    //     }
+
+    //     ).catch(err => { console.log(err) })
+    // }
+
+    // ).catch(err => { console.log(err) })
+
+    try {
+        const All = await Todo.findAll();
+        const truecounter = await Todo.count({ where: { compeleted: true } });
+        const faslecounter = await Todo.count({ where: { compeleted: false } });
+        res.render("index",
+            {
+                pageTitle: "کارهای روزمره",
+                todos: All,
+                truetodo: truecounter,
+                falsetodo: faslecounter
+            }
+        );
+    } catch (error) {
+        console.error(error)
     }
-   
-    ).catch(err => { console.log(err) })
-
 }
